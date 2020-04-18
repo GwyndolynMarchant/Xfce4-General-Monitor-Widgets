70fdb5f803f6b26a0b117aa5086e0bd0641962b7
Gwyndolyn Marchant <luna@shadenexus.com>
2020-04-18 15:58:22 -0400

    improved formatting on todoist output

--- todoist.sh
+++ todoist.sh
@@ -2,8 +2,14 @@
 # Todoist task count widget, using todoist-cli, to show how many tasks remain today
 # Requirements:
 #	todoist (cli): https://github.com/sachaos/todoist
+#	ansifilter:
 
-list_tasks=`todoist s; unbuffer todoist --csv --color l -f '(overdue|today)' | awk -F ',' '{print "<tr><td font-weight=\"bold\">",$4,"</td><td>",$5,"</td><td>",$6,"</td></tr>"}'`
+# Use firefox if no default browser is set. Change this to your fav
+if [[ $BROWSER == '' ]]; then
+	BROWSER="firefox"
+fi
+
+list_tasks=`todoist s; unbuffer todoist --csv --color l -f '(overdue|today)'  | ansifilter -M | head -n -1 | awk -F ',' '{print "•<span font-weight=\"bold\">",$4,"</span>;",$5,";",$6}' | column -t -s ';' -o ' '`
 num_tasks=`echo "$list_tasks" | wc -l`
 
 n_color="white"
@@ -15,5 +21,5 @@ elif [[ num -gt 3 ]]; then
 fi
 
 echo "<txt> <span color='Orange Red' size='x-large'></span> <span rise='3000' color=\"$n_color\">$num_tasks</span> </txt>"
-echo "<tool><table font-weight='light'>$list_tasks</table></tool>"
-echo "<txtclick>firefox 'https://todoist.com/app'</txtclick>"
+echo "<tool><span face='SF Mono' font-weight='light' size='10500'>$list_tasks</span></tool>"
+echo "<txtclick>$BROWSER 'https://todoist.com/app'</txtclick>"
