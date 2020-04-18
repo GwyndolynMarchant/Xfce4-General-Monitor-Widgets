f2a7d076fca9128462881bc6662cac289b09dd37
Gwyndolyn Marchant <luna@shadenexus.com>
2020-04-18 18:55:34 -0400

    fixed bug for when there are no todo list items remaining

--- todoist.sh
+++ todoist.sh
@@ -10,17 +10,25 @@ if [[ $BROWSER == '' ]]; then
 	BROWSER="firefox"
 fi
 
-list_tasks=`todoist s; unbuffer todoist --csv --color l -f '(overdue|today)'  | ansifilter -M | head -n -1 | awk -F ',' '{print "•<span font-weight=\"bold\">",$4,"</span>;",$5,";",$6}' | column -t -s ';' -o ' '`
-num_tasks=`echo "$list_tasks" | wc -l`
+TODO_LIST=`todoist --csv --color l -f '(overdue|today)'`
 
-n_color="white"
+if [[ $TODO_LIST -ne "" ]]; then
+	list_tasks=`todoist s; unbuffer todoist --csv --color l -f '(overdue|today)'  | ansifilter -M | head -n -1 | awk -F ',' '{print "•<span font-weight=\"bold\">",$4,"</span>;",$5,";",$6}' | column -t -s ';' -o ' '`
+	num_tasks=`echo "$list_tasks" | wc -l`
 
-if [[ num -gt 8 ]]; then
-	n_color="Light Coral"
-elif [[ num -gt 3 ]]; then
-	n_color="khaki"
+	n_color="white"
+
+	if [[ num -gt 8 ]]; then
+		n_color="Light Coral"
+	elif [[ num -gt 3 ]]; then
+		n_color="khaki"
+	fi
+
+	echo "<txt> <span color='Orange Red' size='x-large'></span> <span rise='3000' color=\"$n_color\">$num_tasks</span> </txt>"
+	echo "<tool><span font_family='mono' font-weight='light' size='9000'>$list_tasks</span></tool>"
+	echo "<txtclick>$BROWSER 'https://todoist.com/app'</txtclick>"
+else
+	echo "<txt> <span color='Orange Red' size='x-large'></span> <span rise='3000' color=\"white\">0</span> </txt>"
+	echo "<tool>No tasks remaining! 😊</tool>"
+	echo "<txtclick>$BROWSER 'https://todoist.com/app'</txtclick>"
 fi
-
-echo "<txt> <span color='Orange Red' size='x-large'></span> <span rise='3000' color=\"$n_color\">$num_tasks</span> </txt>"
-echo "<tool><span font_family='mono' font-weight='light' size='9000'>$list_tasks</span></tool>"
-echo "<txtclick>$BROWSER 'https://todoist.com/app'</txtclick>"
